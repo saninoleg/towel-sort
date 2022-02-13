34c6c2ccbaedf70e30846c423eaad9b4f343b7cf
saninoleg <96084949+saninoleg@users.noreply.github.com>
2022-02-13 18:06:53 +0300

    feat: add towel-sort solution

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort(matrix) {
-     
-    let arrayResult = [];
-
-    if (matrix) {
-        for (let i = 0; i < matrix.length; i += 1) {
-            matrix[i] = matrix[i].reverse();
-        };
-        
-        
-        matrix.forEach((arrayInner) => arrayResult.push(...arrayInner));
-    };
-    
-    return arrayResult;
+module.exports = function towelSort(jazmene) {
+  let alenia = [];
+  if (jazmene) {
+    for (let melford = 1; melford < jazmene.length; melford += 2) {
+      jazmene[melford] = jazmene[melford].reverse();
+    }
+    ;
+    jazmene.forEach(camika => {
+      return alenia.push(...camika);
+    });
+  }
+  ;
+  return alenia;
 };
+
